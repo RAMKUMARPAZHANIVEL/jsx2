aa564553d9502778bb6ff61a5cc827f7566705ec
Ramkumar <100298314+RAMKUMARPAZHANIVEL@users.noreply.github.com>
2022-11-01 21:49:55 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+  
+        <div className="container App">
+            <div>
+            <h1>The Evolution of <br/>Airbnb's Frontend</h1>
+            <p>@spikebrehm</p>
+            <>
+            <img src="https://cdn-icons-png.flaticon.com/512/8149/8149778.png" alt="no_referer"/>
+            </>
+            
+            <span>airbnb</span>
+            </div>
+        </div>
+
+  );
+}
+
+export default App;
